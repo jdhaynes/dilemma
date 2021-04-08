dbccf6be8257dc73b8103d484b95ffdb7ec68eed
Jack Haynes <jackhaynes94@gmail.com>
2021-04-08 21:08:50 +0100

    Added validation logic for post dilemma command.

--- src/dilemma/Dilemma/DilemmaSvc.Application/Commands/PostDilemmaToTopic/PostDilemmaToTopicCommand.cs
+++ src/dilemma/Dilemma/DilemmaSvc.Application/Commands/PostDilemmaToTopic/PostDilemmaToTopicCommand.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace DilemmaSvc.Application.Commands.PostDilemmaToTopic
 {
-    public class PostDilemmaToTopicCommand
+    public class PostDilemmaToTopicCommand 
     {
-        
+        public Guid TopicId { get; set; }
+        public Guid PosterId { get; set; }
+        public string Question { get; set; }
+        public List<Option> Options { get; set; }
+
+        public class Option
+        {
+            public string Description { get; set; }
+        }
     }
 }
--- src/dilemma/Dilemma/DilemmaSvc.Application/Commands/PostDilemmaToTopic/PostDilemmaToTopicCommandValidator.cs
+++ src/dilemma/Dilemma/DilemmaSvc.Application/Commands/PostDilemmaToTopic/PostDilemmaToTopicCommandValidator.cs
@@ -1,7 +1,29 @@
+using System;
+using FluentValidation;
+
 namespace DilemmaSvc.Application.Commands.PostDilemmaToTopic
 {
-    public class PostDilemmaToTopicCommandValidator
+    public class PostDilemmaToTopicCommandValidator : AbstractValidator<PostDilemmaToTopicCommand>
     {
-        
+        public PostDilemmaToTopicCommandValidator()
+        {
+            RuleFor(x => x.TopicId).NotEqual(Guid.Empty);
+            RuleFor(x => x.PosterId).NotEqual(Guid.Empty);
+            RuleFor(x => x.Question).NotNull();
+            RuleFor(x => x.Question).NotEmpty();
+            RuleFor(x => x.Question).Length(140);
+
+            RuleFor(x => x.Options).NotNull();
+            RuleForEach(x => x.Options).SetValidator(new OptionValidator());
+        }
+
+        public class OptionValidator : AbstractValidator<PostDilemmaToTopicCommand.Option>
+        {
+            public OptionValidator()
+            {
+                RuleFor(x => x.Description).Length(40);
+            }
+        }
     }
+    
 }
